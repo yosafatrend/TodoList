b911455a7005281305ea5da9accbd47abb84f620
siswanto1922 <pujosiswanto131922@gmail.com>
2021-04-20 12:02:38 +0700

    popup_add

--- app/src/main/java/com/yorren/todolist/AddFragment.java
+++ app/src/main/java/com/yorren/todolist/AddFragment.java
@@ -1,5 +1,8 @@
 package com.yorren.todolist;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,8 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class AddFragment extends Fragment {
 
+    FloatingActionButton fabAdd;
+    Dialog popup;
     public AddFragment() {
         // Required empty public constructor
     }
@@ -23,7 +30,21 @@ public class AddFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View v = inflater.inflate(R.layout.fragment_add, container, false);
+        fabAdd = v.findViewById(R.id.fab_add);
+        popup = new Dialog(getContext());
+
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popup.setContentView(R.layout.add_popup);
+                popup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                popup.show();
+            }
+        });
+
+        return v;
+
     }
+
 }
