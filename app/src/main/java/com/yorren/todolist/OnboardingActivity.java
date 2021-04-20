630f29b1ca8b2f080701d58380dbe50d74db45ca
siswanto1922 <pujosiswanto131922@gmail.com>
2021-04-20 16:17:13 +0700

    search layout

--- app/src/main/java/com/yorren/todolist/OnboardingActivity.java
+++ app/src/main/java/com/yorren/todolist/OnboardingActivity.java
@@ -1,14 +1,167 @@
 package com.yorren.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 
 public class OnboardingActivity extends AppCompatActivity {
+
+    private static final int MAX_STEP = 4;
+
+    //memasukan teks dan drawable yg akan digunakan untuk on boarding screen
+    private Button btn_got_it;
+    private String[] title_array = {
+            "To Do List", "Mencacat Kegiatan",
+            "Managemen Waktu", "Apa lagi ya"
+    };
+    private String[] description_array = {
+            "To Do List ni bang papalepapale papale papale paleeeeeee", "Aplikasi ini membantu anda mencatat rencana anda",
+            "Aplikasi ini membantu anda memanagemen waktu lebih baik", "Wes bingung aku arep nulis opo mas"
+    };
+    private int[] about_images_array = {
+            R.drawable.ic_group_8, R.drawable.ic_baseline_event_note_24,
+            R.drawable.ic_baseline_access_time_24, R.drawable.ic_baseline_sticky_note_2_24
+    };
+    private int[] color_array = {
+            R.color.blue, R.color.blue,
+            R.color.blue, R.color.blue
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+        initComponent();
+
+        //menghilangkan status bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    private void initComponent() {
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        btn_got_it = findViewById(R.id.btn_got_it);
+
+        bottomProgressDots(0);
+
+        MyViewPagerAdapter myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+        btn_got_it.setVisibility(View.GONE);
+        btn_got_it.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OnboardingActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_skip).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OnboardingActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void bottomProgressDots(int index) {
+        LinearLayout dotsLayout = findViewById(R.id.layoutDots);
+        ImageView[] dots = new ImageView[MAX_STEP];
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new ImageView(this);
+            int width_height = 15;
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(width_height, width_height));
+            params.setMargins(10, 10, 10, 10);
+            dots[i].setLayoutParams(params);
+            dots[i].setImageResource(R.drawable.dot);
+            dots[i].setColorFilter(getResources().getColor(R.color.colorWhite), PorterDuff.Mode.SRC_IN);
+            dotsLayout.addView(dots[i]);
+        }
+
+        dots[index].setImageResource(R.drawable.dot);
+        dots[index].setColorFilter(getResources().getColor(R.color.colorGold), PorterDuff.Mode.SRC_IN);
+    }
+
+
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(final int position) {
+            bottomProgressDots(position);
+            if (position == title_array.length - 1) {
+                btn_got_it.setVisibility(View.VISIBLE);
+            } else {
+                btn_got_it.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+
+    public class MyViewPagerAdapter extends PagerAdapter {
+
+        MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(R.layout.item_intro, container, false);
+            ((TextView) view.findViewById(R.id.title)).setText(title_array[position]);
+            ((TextView) view.findViewById(R.id.description)).setText(description_array[position]);
+            ((ImageView) view.findViewById(R.id.image)).setImageResource(about_images_array[position]);
+            view.findViewById(R.id.lyt_parent).setBackgroundColor(getResources().getColor(color_array[position]));
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return title_array.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
     }
 }
